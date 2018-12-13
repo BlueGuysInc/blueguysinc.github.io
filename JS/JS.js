745425215a227fa1b9bcbfd6e2fabddfebc02492
TvoyOtec <tltxdmin@gmail.com>
2018-12-13 22:23:22 +0300

    0.2

--- JS/JS.js
+++ JS/JS.js
@@ -53,33 +53,92 @@ icon5.addEventListener('click', function(e) {
     icon5.style.background = "black";
 });
 
+ let slider2 = document.getElementById('divSlider2');
+let icon6 = document.getElementById('divIcon6');
+let icon7 = document.getElementById('divIcon7');
+let icon8 = document.getElementById('divIcon8');
+let icon9 = document.getElementById('divIcon9');
+let icon10 = document.getElementById('divIcon10');
+
+icon6.addEventListener('click', function(e) {
+    slider2.classList.remove('img7', 'img8', 'img9', 'img10');
+    slider2.classList.add('img6');
+    icon7.style.background = "gray";
+    icon8.style.background = "gray";
+    icon9.style.background = "gray";
+    icon10.style.background = "gray";
+    icon6.style.background = "black";
+});
+icon7.addEventListener('click', function(e) {
+    slider2.classList.remove('img6', 'img8', 'img9', 'img10');
+    slider2.classList.add('img7');
+    icon6.style.background = "gray";
+    icon8.style.background = "gray";
+    icon9.style.background = "gray";
+    icon10.style.background = "gray";
+    icon7.style.background = "black";
+
+});
+icon8.addEventListener('click', function(e) {
+    slider2.classList.remove('img6', 'img7', 'img9', 'img10');
+    slider2.classList.add('img8');
+    icon6.style.background = "gray";
+    icon7.style.background = "gray";
+    icon9.style.background = "gray";
+    icon10.style.background = "gray";
+    icon8.style.background = "black";
+});
+icon9.addEventListener('click', function(e) {
+    slider2.classList.remove('img6', 'img7', 'img8', 'img10');
+    slider2.classList.add('img9');
+    icon6.style.background = "gray";
+    icon7.style.background = "gray";
+    icon8.style.background = "gray";
+    icon10.style.background = "gray";
+    icon9.style.background = "black";
+});
+icon10.addEventListener('click', function(e) {
+    slider2.classList.remove('img6', 'img7', 'img8', 'img9');
+    slider2.classList.add('img10');
+    icon6.style.background = "gray";
+    icon7.style.background = "gray";
+    icon8.style.background = "gray";
+    icon9.style.background = "gray";
+    icon10.style.background = "black";
+});
+
 
 //function invisiblу
  var cunt = document.getElementById("content");
+ var menu = document.getElementById("Menu1");
+ var men = document.getElementsByClassName("menu")
 function viewDiv(){
+     cunt2.style.display = "none";
  if(cunt.style.display == "none"){
         cunt.style.display = "block";
+        menu.style.background = "lightgray";
+        menu2.style.background = "#E6E6E6";
+        
     }  else {
         cunt.style.display = "none";
- };
+         menu.style.background = "#E6E6E6";
+};
 };
 
+var cunt2 = document.getElementById("content2");
+var menu2 = document.getElementById("Menu2");
+function viewDiv2(){
+ cunt.style.display = "none";
+ if(cunt2.style.display == "none"){
+        cunt2.style.display = "block";
+        menu2.style.background = "lightgray";
+        menu.style.background = "#E6E6E6";
 
-
-/*
-let slider-m = document.getElementById('');
-let  = document.getElementById('');
-let  = document.getElementById('');
-let  = document.getElementById('');
-let  = document.getElementById('');
-let  = document.getElementById('');
-*/
-
-
-
-
-
-
+    }  else {
+        cunt2.style.display = "none";
+        menu2.style.background = "#E6E6E6";
+ };
+};
 
 
 
